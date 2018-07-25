f7aa3df42e033923f4ae3a914177d266b6ed22a1
Shudipta <alittleprogramming@gmail.com>
2018-07-25 19:36:15 +0600

    Add installation doc

--- hack/dev/build.sh
+++ hack/dev/build.sh
@@ -9,12 +9,88 @@ export IMG=db-broker
 export TAG=try-for-memcached
 export ONESSL=
 
-export NAME=my-broker
-export NAMESPACE=db-broker
+export NAME=service-broker
+export NAMESPACE=service-broker
 export SERVICE_ACCOUNT="$NAME"
-export APP=db-broker
-export IMAGE_PULL_POLICY=Always
-#export IMAGE_PULL_POLICY=IfNotPresent
+export APP=service-broker
+export IMAGE_PULL_POLICY=IfNotPresent
+
+export KUBEDB_ENV=${KUBEDB_ENV:-prod}
+export SCRIPT_LOCATION="curl -fsSL https://raw.githubusercontent.com/kubedb/service-broker/master/"
+if [ "$KUBEDB_ENV" = "dev" ]; then
+    export SCRIPT_LOCATION="cat "
+    export TAG=$TAG
+    export IMAGE_PULL_POLICY=Always
+fi
+
+export CMD=
+
+show_help() {
+    echo "build.sh"
+    echo " "
+    echo "build.sh [commands] [options]"
+    echo " "
+    echo "commands:"
+    echo "---------"
+    echo "build            builds and push the docker image for service-broker"
+    echo "run              installs service-broker"
+    echo "uninstall        uninstalls service-broker"
+    echo " "
+    echo "options:"
+    echo "--------"
+    echo "-h, --help                         show brief help"
+    echo "-n, --namespace=NAMESPACE          specify namespace (default: $NAMESPACE)"
+    echo "    --docker-registry              docker registry used to pull stash images (default: $DOCKER_REGISTRY)"
+}
+
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        build|run|uninstall)
+            export CMD=$1
+            shift
+            ;;
+        -n)
+            shift
+            if test $# -gt 0; then
+                export NAMESPACE=$1
+            else
+                echo "no namespace specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --namespace*)
+            export NAMESPACE=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        --docker-registry*)
+            export DOCKER_REGISTRY=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        *)
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+echo "DOCKER_REGISTRY=$DOCKER_REGISTRY"
+echo "IMG=$IMG"
+echo "TAG=$TAG"
+echo "ONESSL=$ONESSL"
+echo "NAME=$NAME"
+echo "NAMESPACE=$NAMESPACE"
+echo "SERVICE_ACCOUNT=$SERVICE_ACCOUNT"
+echo "APP=$APP"
+echo "IMAGE_PULL_POLICY=$IMAGE_PULL_POLICY"
+echo ""
+echo "KUBEDB_ENV=$KUBEDB_ENV"
+echo "SCRIPT_LOCATION=$SCRIPT_LOCATION"
+echo ""
 
 build() {
     pushd $REPO_ROOT
@@ -63,32 +139,7 @@ ensure_onessl() {
     fi
 }
 
-parse_flags() {
-    while test $# -gt 0; do
-        case "$1" in
-            -n)
-                shift
-                if test $# -gt 0; then
-                    export NAMESPACE=$1
-                else
-                    echo "no namespace specified"
-                    exit 1
-                fi
-                shift
-                ;;
-            --namespace*)
-                export NAMESPACE=`echo $1 | sed -e 's/^[^=]*=//g'`
-                shift
-                ;;
-            --docker-registry*)
-                export DOCKER_REGISTRY=`echo $1 | sed -e 's/^[^=]*=//g'`
-                shift
-                ;;
-        esac
-    done
-}
-
-deploy_db_broker() {
+deploy_service_broker() {
     local found=0
     ns=($(kubectl get ns -o jsonpath='{range .items[*]}{.metadata.name} {end}'))
     for n in "${ns[@]}"; do
@@ -100,24 +151,23 @@ deploy_db_broker() {
         kubectl create ns $NAMESPACE
     fi
 
-    cat hack/dev/deployment.yaml | $ONESSL envsubst | kubectl apply -f -
-    cat hack/dev/service.yaml | $ONESSL envsubst | kubectl apply -f -
-    cat hack/dev/rbac.yaml | $ONESSL envsubst | kubectl create -f -
-    cat hack/dev/cluster_service_broker.yaml | $ONESSL envsubst | kubectl apply -f -
+    ${SCRIPT_LOCATION}hack/dev/deployment.yaml | $ONESSL envsubst | kubectl apply -f -
+    ${SCRIPT_LOCATION}hack/dev/service.yaml | $ONESSL envsubst | kubectl apply -f -
+    ${SCRIPT_LOCATION}hack/dev/rbac.yaml | $ONESSL envsubst | kubectl apply -f -
+    ${SCRIPT_LOCATION}hack/dev/cluster_service_broker.yaml | $ONESSL envsubst | kubectl apply -f -
 
     echo
-    echo "waiting until db-broker deployment is ready"
-    $ONESSL wait-until-ready deployment $NAME --namespace $NAMESPACE || { echo "db-broker deployment failed to be ready"; exit 1; }
+    echo "waiting until service-broker deployment is ready"
+    $ONESSL wait-until-ready deployment $NAME --namespace $NAMESPACE || { echo "service-broker deployment failed to be ready"; exit 1; }
 
     echo
-    echo "Successfully installed db-broker in $NAMESPACE namespace!"
+    echo "Successfully installed service-broker in $NAMESPACE namespace!"
 }
 
 run() {
     pushd $REPO_ROOT
         ensure_onessl
-        parse_flags
-        deploy_db_broker
+        deploy_service_broker
     popd
 }
 
@@ -127,11 +177,11 @@ uninstall() {
         kubectl delete service -l app=$APP --namespace $NAMESPACE
         kubectl delete deployment -l app=$APP --namespace $NAMESPACE
         # delete RBAC objects, if --rbac flag was used.
-        kubectl delete clusterrolebindings -l app=$APP
         kubectl delete serviceaccount -l app=$APP --namespace $NAMESPACE
+        kubectl delete clusterrolebindings -l app=$APP
 
         echo
-        echo "waiting for db-broker pod to stop running"
+        echo "waiting for service-broker pod to stop running"
         for (( ; ; )); do
             pods=($(kubectl get pods --all-namespaces -l app=$APP -o jsonpath='{range .items[*]}{.metadata.name} {end}'))
             total=${#pods[*]}
@@ -145,9 +195,11 @@ uninstall() {
         kubectl delete ns $NAMESPACE
 
         echo
-        echo "Successfully uninstalled db-broker!"
+        echo "Successfully uninstalled service-broker!"
         exit 0
     popd
 }
+
+$CMD
 
 "$@"
