5bfaca8ff6dc432d7271baf9210a303027446f13
shudipta <shudipta@appscode.com>
2018-12-05 18:36:00 +0600

    Add hack/docker/make.sh to build and push docker image

---
+++ hack/docker/make.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GOPATH=$(go env GOPATH)
+SRC=$GOPATH/src
+BIN=$GOPATH/bin
+ROOT=$GOPATH
+REPO_ROOT=$GOPATH/src/github.com/appscode/service-broker
+
+source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+
+APPSCODE_ENV=${APPSCODE_ENV:-dev}
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-appscode}
+IMG=service-broker
+
+DIST=$REPO_ROOT/dist
+mkdir -p $DIST
+if [ -f "$DIST/.tag" ]; then
+  export $(cat $DIST/.tag | xargs)
+fi
+
+build_binary() {
+  pushd $REPO_ROOT
+  ./hack/builddeps.sh
+  ./hack/make.py build service-broker
+  detect_tag $DIST/.tag
+  popd
+}
+
+build_docker() {
+  pushd $REPO_ROOT/hack/docker
+  cp $DIST/service-broker/service-broker service-broker
+  chmod 755 service-broker
+
+  cat >Dockerfile <<EOL
+FROM ubuntu
+
+COPY service-broker /bin/service-broker
+EOL
+  local cmd="docker build --pull -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  echo $cmd; $cmd
+
+  rm service-broker Dockerfile
+  popd
+}
+
+build() {
+  build_binary
+  build_docker
+}
+
+docker_push() {
+  if [ "$APPSCODE_ENV" = "prod" ]; then
+    echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" = "git_tag" ]; then
+    echo "Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  hub_canary
+}
+
+docker_release() {
+  if [ "$APPSCODE_ENV" != "prod" ]; then
+    echo "'release' only works in PROD env."
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" != "git_tag" ]; then
+    echo "'apply_tag' to release binaries and/or docker images."
+    exit 1
+  fi
+  hub_up
+}
+
+source_repo $@
